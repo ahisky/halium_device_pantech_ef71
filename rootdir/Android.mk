e07e5f689c8bf8a4cc1d852c2a15e03c6a2d7fc4
alpha2x <hexagonwin@disroot.org>
2021-07-18 22:03:39 +0900

    More wifi fixes

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -106,10 +106,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
 include $(BUILD_PREBUILT)
 
+#WIFI
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init-wifi.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init-wifi.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
